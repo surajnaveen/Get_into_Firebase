e6e8c6e56f38305b69b0bd027a1f2902bbb6ab98
surajnaveen <suraj0nb@gmail.com>
2024-08-13 14:13:23 +0530

    add fuction file and database connection

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import {add} from "./function.js"
+
+console.log(add(10,6))
+
+const appSettings = {
+    DatabaseURL: "https://playground-bd4eb-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
 const Button = document.getElementById("add-button")
 const InputField = document.getElementById("input-field")
 
